0cbf0cd9c749da6f74bdcaa42d67e2e77203b0b7
ben ko <cskch99@gmail.com>
2017-12-14 00:20:16 +0800

    eee

--- MSBDGroupProject/project3.py
+++ MSBDGroupProject/project3.py
@@ -53,6 +53,73 @@ def readImage(X_path , y_path):
 def splitImage(srcDir, dstDir, imageSize) :
 #we need split the large image
 
+def tfInit() :
+    config = tf.ConfigProto()
+    config.gpu_options.per_process_gpu_memory_fraction = 0.7
+    config.gpu_options.allow_growth = True
+    set_session(tf.Session(config=config))
+
+def filePrefixWith(folder, filePrefix) :
+    for filename in os.listdir(folder) : 
+        if filename.startswith(filePrefix)  :
+            return folder + "/" + filename;
+
+    return "";
+
+def generate_arrays_from_file(path, batchSize, class_count):
+    batchCount = 0
+
+    inputs = []
+    targets = []
+    weights = []
+    while 1:
+        f = open(path)
+        for line in f:
+
+            aLine = line.split('\t')
+
+            image_path = aLine[0];
+            label = int(aLine[1]);
+            weight = float(aLine[2]);
+
+            if (label == 0) :
+                weight = 1 
+            else :
+                weight = weight * 2
+
+
+            # create numpy arrays of input data
+            # and labels, from each line in the file
+            #x, y = process_line(line)
+            #img = image.load_img(x)
+            img = image.load_img(image_path)
+            x = image.img_to_array(img)
+            x = np.expand_dims(x, axis=0)
+            x = preprocess_input(x)
+            batchCount += 1
+            
+            ground_truth = np.zeros(class_count, dtype=np.float32)
+            ground_truth[label] = 1;
+
+            targets.append(ground_truth)
+            inputs.append(x[0])
+
+            weights.append(weight)
+
+            if batchCount >= batchSize:
+                batchCount = 0
+                X = np.array(inputs)
+                y = np.array(targets)
+                w = np.array(weights)
+
+                inputs = []
+                targets = []
+                weights = []
+                yield X, y              #, w
+
+        f.close()
+
+
 def EMTrain(epochs, dataFolder, inputFile, resultFile) :
 #implement the EM training
 
