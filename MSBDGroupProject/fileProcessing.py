d33168e4795b9c1f67a9965744beeb0691df0f64
HahaLun <33598197+HahaLun@users.noreply.github.com>
2017-12-14 09:25:12 +0800

    process file

--- MSBDGroupProject/fileProcessing.py
+++ MSBDGroupProject/fileProcessing.py
@@ -1,12 +1,12 @@
 
-import os; 
+import os;
 
 # 3. Import libraries and modules
 import numpy as np
 #np.random.seed(123)  # for reproducibility
 import json
 
-import glob 
+import glob
 from sklearn.metrics import classification_report
 from skimage import io
 from os import listdir
@@ -14,3 +14,8 @@ from os.path import isfile, join
 import glob
 import ntpath
 
+def fileProcessing():
+    dataFolder = "./data"
+    trainData = np.loadtxt("./train.txt", dtype="str", delimiter='\t' )
+    testData = np.loadtxt("./test.txt", dtype="str",  delimiter='\t' )
+    return trainData , testData
